f8ff56a5fcdfe91964a504eafe92a0678bd9d497
Chase Tucker <tuck8106@gmail.com>
2020-07-02 12:27:16 -0700

    added recent reads section

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
+"use strict";
+
 const express = require("express");
+var geolocation = require("geolocation");
 
 const app = express();
 
 const port = 3000;
+
+// app.use(express.static("public"));
+
+app.get("/", function (req, res) {
+  console.log(req);
+  geolocation.getCurrentPosition(function (err, position) {
+    if (err) throw err;
+    console.log(position);
+  });
+  res.send(".public/index");
+});
 
 app.get("/", (req, res) => res.send("Testing my Geolocation Node app!"));
 
